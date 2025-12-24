2e3bb9ede8d239d46605f517e8b7b1a6e03d4a40
AkshitaSharma@18 <sharmaakshita099@gmail.com>
2025-12-24 13:12:59 +0530

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+
+   return(
+     <div className="card">
+        <img className="card-img" src="https://img.freepik.com/free-vector/girl-wearing-beret-anime-character-poster_603843-2524.jpg?semt=ais_hybrid&w=740&q=80" alt="profile Picture"></img>
+        <h2 className="card-title" >Akshita Sharma</h2>
+        <p className="card-text" >I am a future software developer</p>
+     </div>
+   );
+}
+
+export default Card
